c8b8943821c8724ffa96bd14fbcf94803abdf77e
SoftcraftSolution <168642737+SoftcraftSolution@users.noreply.github.com>
2024-08-27 18:10:32 +0530

    reward hisstory done

--- src/components/coupon.js
+++ src/components/coupon.js
@@ -1,29 +1,53 @@
 import React from 'react';
-import './coupon.css'; // Import the CSS file for styling
-import kfcLogo from './kfclogo.png'; // Assuming you have the KFC logo saved locally
-import confettiImage from './conf.png'; // Assuming you have a confetti image saved locally
+import './coupon.css';
+import { useLocation } from 'react-router-dom';
+import Confetti from 'react-confetti';
+import giftImage from './conf.png';
 
 function Coupon() {
+  const { state } = useLocation(); // Get the passed state
+  const couponData = state?.couponData || {}; // Extract coupon data
+
+  const { offer, issueDate, expiredDate, couponCode } = couponData;
+  console.log(couponData);
+
+  // Helper function to format date
+  const formatDate = (date) => {
+    const options = {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true
+    };
+    return new Intl.DateTimeFormat('en-US', options).format(new Date(date));
+  };
+
   return (
-    <div className="coupon-container">
-      <div className="coupon-header">
-       
-        <h1>Exciting Reward for you</h1>
-      </div>
-      <div className="coupon-body">
-        <img src={kfcLogo} alt="KFC Logo" className="kfc-logo" />
-        <h2>25% OFF</h2>
-        <p>Get 25% OFF at your next KFC buy</p>
-        <ul>
-          <li>Redeemable at all KFC restaurants in India.</li>
-          <li>Not valid with any other discounts and promotions.</li>
-          <li>The coupon is non-transferable and can only be used by the person who received it.</li>
-        </ul>
-      </div>
-      <div className="coupon-footer">
-        <h3>Your coupon code</h3>
-        <div className="coupon-code">DH3YHZXB</div>
-        <p>Valid till 3 August, 2024</p>
+    <div className="container">
+         <Confetti />
+         <div className='imgHead'>
+          <img src={giftImage} className='imgHead-img'></img>
+          <div id='imgHead-header'>Exciting Reward for you</div>
+         </div>
+      <div className="coupon-card">
+        <div className="discount">{offer}</div>
+        <p>Get {offer} at your next purchase</p>
+        <div className="coupon-body">
+          
+          <ul>
+            <li>Redeemable at all locations.</li>
+            <li>Not valid with any other discounts and promotions.</li>
+            <li>The coupon is non-transferable and can only be used by the person who received it.</li>
+          </ul>
+          
+        </div>
+        <div className="coupon-code">
+            <div id='titleCoupon'>Your coupon code</div>
+            <span>{couponCode}</span>
+          </div>
+        <div className="coupon-footer">Valid till {formatDate(expiredDate)}</div>
       </div>
     </div>
   );
